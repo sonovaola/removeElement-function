d1b41feedfd2f250ee142e4387e3af4a149c2e36
sonovaola <sonovaola@gmail.com>
2023-01-06 00:18:35 +0200

    Implement the removeElement function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+removeElement = function(array,item) {
+    let a = 5;
+    for (a in array) {
+        if (array[a] === item) {
+            array.splice(a, 1);
+            break;
+            }
+        }
+        return array;
+    };
+   
+    const array = [1, 2, 3, 4, 5, 6, 7];
+    removeElement(array, 5);
+    console.log(array)
